de4d27d5dc291a1cac28fa87ad71c7e4d3c7c41d
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-11-13 21:22:43 +0530

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,31 @@
+// public class Largest {
+//     public static void main(String[] args) {
+//         int a = 10, b = 25, c = 15;
+//         if (a >= b && a >= c)
+//             System.out.println("Largest: " + a);
+//         else if (b >= a && b >= c)
+//             System.out.println("Largest: " + b);
+//         else
+//             System.out.println("Largest: " + c);
+//     }
+// }
+import java.util.Scanner;
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        if (a >= b && a >= c)
+            System.out.println("Largest: " + a);
+        else if (b >= a && b >= c)
+            System.out.println("Largest: " + b);
+        else
+            System.out.println("Largest: " + c);
+        sc.close(); 
+    }
+}
+
