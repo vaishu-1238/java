69aa7ad55db0ff8efa2c1650aceeeef1898bba5b
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-11-13 21:36:49 +0530

    Add files via upload

---
+++ CompoundOperatorsDemo.java
@@ -0,0 +1,12 @@
+public class CompoundOperatorsDemo {
+    public static void main(String[] args) {
+        int num = 10;
+        System.out.println("Initial value: " + num);
+        num += 5; 
+        System.out.println("After += 5: " + num);
+        num -= 3; 
+        System.out.println("After -= 3: " + num);
+        num *= 2; 
+        System.out.println("After *= 2: " + num);
+    }
+}
