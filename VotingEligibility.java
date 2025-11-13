39e1600bf8659b7ad4a3dff315e33dc7cfa6ad38
vaishu-1238 <vaishnavideshpande995@gmail.com>
2025-11-13 21:35:05 +0530

    Add files via upload

---
+++ VotingEligibility.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class VotingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        System.out.print("Are you a citizen? (true/false): ");
+        boolean isCitizen = sc.nextBoolean();
+        if (age >= 18 && isCitizen) {
+            System.out.println("You are eligible to vote.");
+        } else {
+            System.out.println("You are not eligible to vote.");
+        }
+        sc.close();
+    }
+}
+
